60789e47ea84b10c46ce6e53967f0e5a5a2e3dd9
Luis Cortes <lacortes@cpp.edu>
2017-11-27 21:26:26 -0800

    first

---
+++ test.js
@@ -0,0 +1,21 @@
+var dog = {
+    name: "bob"
+};
+
+var hello = "hello";
+
+var tiger = [
+    {name: "LEO"},
+    {name: "Oscar"}
+];
+
+// var kitties =  [
+//     {breed_type: "abyssinian",
+//     info: "Abyssinians are extraordinary pets. While it is interesting to learn about the breeding purpose of Abyssinians, their genetics actually influence health, outward appearance and behavior. Some behaviors make the Abyssinian and some can be quite irritating! Understanding her unique needs will help you keep her healthy and will create a stronger bond between the two of you. ",
+//     length: "3",
+//     grooming: "2",
+//     energy: "5",
+//     training: "4",
+//     affection: "4",
+//     friendliness: "5"}
+// ];
